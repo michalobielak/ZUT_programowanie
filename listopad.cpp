ff19d94df95f5cde00b02d02e528584d89b44830
Michal Obielak <mobielak7@gmial.com>
2018-11-18 14:27:18 +0100

    Zapoznać się z instrukcjami warunkowymi (if, else if, else oraz switch). Pobrać od używkownika liczbę i wyświetlić informację, czy jestona: mniejsza, większa, czy równa 0

--- listopad.cpp
+++ listopad.cpp
@@ -25,14 +25,35 @@ void zadanie1_2()
 //Pobrac od uzytkownika liczbe x. Wyswietlic kwadrat liczby x.
 void zadanie1_3()
 {
+	cout<<"Zadanie 1.3"<<endl;
 	int x;
 	cout<<"Wpisz liczbe: ";
 	cin>>x;
 	cout<<"Kwadrat podanej liczby to: "<<pow(x, 2)<<endl;
 }
+
+//Zapoznac sie z instrukcjami warunkowymi (if, else if, else oraz switch).
+//Pobrac od uzywkownika liczbe i wyswietlic informacje, czy jest ona: mniejsza, wieksza, czy rowna 0
+void zadanie1_4()
+{
+	cout<<"Zadanie 1.4"<<endl;
+	int x;
+	cout<<"Wpisz liczbe: ";
+	cin>>x;
+	cout<<"Podana liczba jest ";
+	if (x < 0) {
+		cout<<"mniejsza od zera";
+	} else if (x > 0) {
+		cout<<"wieksza od zera";
+	} else {
+		cout<<"rowna zero";
+	}
+	cout<<endl;
+}
 int main() {
 	zadanie1_1();
 	zadanie1_2();
 	zadanie1_3();
+	zadanie1_4();
 	return 0;
 }
