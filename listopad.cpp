3e3bae85f7430b90f5b6efa3f859fd2af8fc81fe
Michal Obielak <mobielak7@gmial.com>
2018-11-18 14:04:14 +0100

    1.1. Zadeklarować zmienne typu int, float, char. Przypisać do nich wartość, wyświetlić je na ekran.

---
+++ listopad.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Zadeklarowac zmienne typu int, float, char. Przypisac do nich wartosc, wyswietlic je na ekran
+void zadanie1_1 ()
+{
+	int i = 1;
+	float f = 1.1;
+	char c = 'a';
+	cout<<"wartosc zmiennej typu int: "<<i<<endl;
+	cout<<"wartosc zmiennej typu float: "<<f<<endl;
+	cout<<"wartosc zmiennej typu char: "<<c<<endl;
+}
+int main() {
+	zadanie1_1();
+	return 0;
+}
