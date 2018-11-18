705b5dacf10db1489c3ef796bf2334ef0817fbb3
Michal Obielak <mobielak7@gmial.com>
2018-11-18 14:08:50 +0100

    Wyświetlić zmienną typu int na ekran. Należy użyć metody printf i konstrukcji %d

--- listopad.cpp
+++ listopad.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Zadeklarowac zmienne typu int, float, char. Przypisac do nich wartosc, wyswietlic je na ekran
 void zadanie1_1 ()
 {
+	cout<<"Zadanie 1.1"<<endl;
 	int i = 1;
 	float f = 1.1;
 	char c = 'a';
@@ -11,7 +12,15 @@ void zadanie1_1 ()
 	cout<<"wartosc zmiennej typu float: "<<f<<endl;
 	cout<<"wartosc zmiennej typu char: "<<c<<endl;
 }
+
+//Wyswietlic zmienna typu int na ekran. Nalezy uzyc metody printf i konstrukcji %d
+void zadanie1_2(){
+	cout<<"Zadanie 1.2"<<endl;
+	int i = 1;
+	printf("wartosc zmiennej typu int: %d \n", i);
+}
 int main() {
 	zadanie1_1();
+	zadanie1_2();
 	return 0;
 }
