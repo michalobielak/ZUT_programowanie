a58ee70c0705a3300a5f59aa99d237038c7ac724
michalo100 <michalo100@o2.pl>
2019-01-06 22:35:57 +0100

    realizacja zadania 4

--- grudzien.cpp
+++ grudzien.cpp
@@ -62,15 +62,43 @@ bool containsDigits(char *tab){
 	int dlugosc = getLength(tab);
 	for(int i = 0; i < dlugosc; i++){
 		if((int)tab[i] >= 48 && (int)tab[i] <= 57){
-			cout << i << endl;
-			cout << tab[i];
 			return true;
 		}
 	}
 	return false;
 }
+
+//zad 4.3 Napisaæ metodê bool AreArraysldentical(int * al, int sl, int * a2, int s2) zwracaj¹c¹ informacjê, czy dwie tablice
+//podane jako parametr s¹ identyczne, tj. czy maj¹ tak¹ sam¹ d³ugoœæ i czy na ka¿dym indeksie wystêpuje taki sam
+//element.
+bool AreArraysIdentical(int *a1, int s1, int *a2, int s2){
+	if(s1 == s2){
+		for(int i = 0; i < s1; i++){
+			if (a1[i] != a2[i])
+			return false;
+		}
+		return true;
+	} else {
+		return false;
+	}
+} 
+
+//zad 4.4 Napisaæ metodê char * reverseString(char*s), która ZWRÓCI (a nie wypisze!) string, który jest odwróconym
+//stringiem przekazanym jako pierwszy parametr. Funkcja zaalokuje pamiêæ na nowy ³añcuch i go zwróci. (ca³y
+//algorytm nale¿y zaimplementowaæ samodzielnie). Nale¿y go nastêpnie „przechwyciæ" w funkcji main, aby pamiêæ na
+//koniec wyczyœciæ
+char *reverseString(char *s){
+	int x = getLength(s);
+	int j = 0;
+	char *wynik;
+	wynik = (char *)malloc(x * sizeof(char));
+	for(int i = x-1; i >= 0; i--){
+		wynik[j] = s[i];
+		j++; 
+	}
+	return wynik;
+}
 int main() {
-	/*
 	// zad 1.1 Utworzyæ 5-cio elementow¹ tablicê typu int. Pobraæ od u¿ytkownika 5 elementów i dodaæ je do tablicy.
 	int x;
 	int wymiar = 5;
@@ -134,15 +162,92 @@ int main() {
 			}
 		}
 	}
-	*/
-	char *str = "Hello!";
+	
+	
+	//Zad 3.4 Pobraæ od u¿ytkownika ³añcuch znaków (dowolna metoda). Przekazaæ go do ka¿dej z metod z 3. czêœci zadañ
+	char str[5];
+	cout << "Podaj lancuch znakow" << endl;
+	for(int i = 0; i < 5; i++) {
+    	cout << "Znak o id " << i <<": ";
+    	cin >> str[i];
+	}
+	cout << str;
+	// metoda z 3.1
 	cout << "dlugosc slowa " << str << " to: " << getLength(str) << endl;
+	// metoda z 3.2
 	Reverse(str);
 	cout << endl;
+	// metoda z 3.3
 	if(containsDigits(str)){
 		cout << "jest liczba";
 	} else {
 		cout << "nie ma liczby";
+	}
+	
+	
+	//zad 4.1 Pobraæ od u¿ytkownika liczbê n. Zaalokowaæ pamiêæ na n elementów int i pobraæ je od u¿ytkownika. Wykonaæ na niej metodê printBackwards z 2.2. 
+	{
+		int n;
+		cout << "Podaj wymiar tablicy: ";
+		cin >> n;
+		int *tab;
+    	tab = (int *)malloc(n * sizeof(int));
+    	for(int i = 0; i < n; i++){
+    		cout << "Podaj element " << i+1 << ": ";
+    		cin >> tab[i];
+		}
+		printBackwards(tab, n);
+	}
+
+	//zad 4.2 Pobraæ od u¿ytkownika liczbê n. Zaalokowaæ pamiêæ na n elementów int i pobraæ je od u¿ytkownika. Nastêpnie
+	//zaalokowaæ pamiêæ i utworzyæ tablicê, w której bêd¹ tylko UJEMNE wartoœci z pierwszej tablicy. Na koniec zwolniæ
+	//pamiêæ na obie tablice. 
+	{
+		int n;
+		int licznik = 0;
+		cout << "Podaj wymiar tablicy: ";
+		cin >> n;
+		int *tab;
+		int *tab2;
+    	tab = (int *)malloc(n * sizeof(int));
+    	for(int i = 0; i < n; i++){
+    		cout << "Podaj element " << i+1 << ": ";
+    		cin >> tab[i];
+		}
+		for(int i = 0; i < n; i++){
+			if(tab[i] < 0) {
+				licznik++;
+			}
+		}
+		if (licznik > 0) {
+			tab2 = (int *)malloc(licznik * sizeof(int));
+			int j = 0;
+			for(int i = 0; i < n; i++){
+			if(tab[i] < 0) {
+					tab2[j] = tab[i];
+					j++;
+				}
+			}
+		}
+		cout << "liczby ujemne: ";
+		for(int i = 0; i < licznik; i++){
+			cout << tab2[i] << ", ";
+		}
+		free(tab);
+		free(tab2);
+	}
+	
+	{
+		char *str;
+		str = (char *)malloc(10 * sizeof(char));
+		for(int i = 0; i < 10; i++){
+			cout << "Podaj element " << i+1 << ": ";
+    		cin >> str[i];
+		}
+		char * result = reverseString(str);
+		printf("%s", result);
+		free(str);
+		free(result);
 	}
 	return 0;
 }
